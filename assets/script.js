efb11b1e2f75b11bd6103c847b817939505a6f4f
Gaëtan Brun <brungaetan@outlook.fr>
2023-09-04 15:49:37 +0200

    Ajout des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,50 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// FLECHES SLIDER
+
+// Déclartion de la variable pour suivre l'index de l'image actuelle
+let currentIndex = 0;
+
+// Obtention de la référence à l'image du carrousel
+const carouselImage = document.querySelector(".banner-img");
+
+// Obtention de la référence aux flèches du carrousel
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+
+// Fonction pour changer l'image en fonction de l'index
+function changeImage(index) {
+    carouselImage.src = `./assets/images/slideshow/${slides[index].image}`;
+    document.querySelector("#banner p").innerHTML = slides[index].tagLine;
+}
+
+// Ajout d'un event listener pour le clic sur la flèche gauche
+leftArrow.addEventListener("click", function () {
+	console.log("Clic sur la flèche gauche");
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+
+    changeImage(currentIndex);
+	updateBulletPoints(currentIndex); // Mettre à jour les bullet points
+});
+
+// Ajout d'un event listener pour le clic sur la flèche droite
+rightArrow.addEventListener("click", function () {
+	console.log("Clic sur la flèche droite");
+    currentIndex++;
+
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    changeImage(currentIndex);
+	updateBulletPoints(currentIndex); // Mettre à jour les bullet points
+});
+
+// Initialisation de l'image avec la première diapositive
+changeImage(currentIndex);
